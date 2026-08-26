72384fb35d32b39cb84e2d383a32b75ba0528217
RaihanFAU <ahmraihan.aiub@gmail.com>
2026-08-26 20:28:20 +0200

    Initial Flask learning project

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return "Welcome to the Flask app!"
+
+@app.route('/index')
+def index():
+    return "Welcome to the index page!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
